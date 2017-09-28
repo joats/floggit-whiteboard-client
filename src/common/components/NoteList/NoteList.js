fa3098ac99bb1df56490fce3d66ce2d099359b80
Joakim Holmgren <joakim00n@gmail.com>
2017-09-28 15:20:01 +0200

    10_notelist-component (#4)
    *added notelist component
    
    *added notelist proptypes
    
    *added rule for extraenous

---
+++ src/common/components/NoteList/NoteList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import noteListProps from './NoteList.props';
+
+const NoteList = props => (
+  <ul>
+    {
+      props.notes.map(notes => (
+        <li key={notes.id}>
+            key={notes.id}
+          {notes.id}
+          {notes.title}
+          {notes.infoList.map(info => info.value)}
+          {notes.color}
+        </li>
+      ))
+    }
+  </ul>
+);
+NoteList.propTypes = noteListProps;
+
+
+export default NoteList;
