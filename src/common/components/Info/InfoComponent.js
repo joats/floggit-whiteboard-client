8f15574ebd963e9a3005c9da218950116b57aa57
Daniel Kemter <danne2170@gmail.com>
2017-09-28 13:38:35 +0200

    created info-component

---
+++ src/common/components/Info/InfoComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import infoProps from './InfoComponent.props';
+
+const Info = props => (
+  <div className="info">
+    <h1>{props.header}</h1>
+    <img className="logo" src={props.logo} alt="floggit!" />
+    <div>{props.user}</div>
+    <div>{props.department}</div>
+  </div>
+);
+
+Info.propTypes = infoProps;
+
+export default Info;
