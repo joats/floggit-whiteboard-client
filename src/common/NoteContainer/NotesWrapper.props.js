f91c51d3a1645c0b1fc6da126619bdfb525abf3e
DanielBFlores <d2_88@hotmail.com>
2017-10-05 14:36:29 +0200

    Update NotesWrapper.props.js

--- src/common/NoteContainer/NotesWrapper.props.js
+++ src/common/NoteContainer/NotesWrapper.props.js
@@ -1,17 +1,1 @@
-import PropTypes from 'prop-types';
 
-const props = {
-  notes: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    infoList: PropTypes.arrayOf(PropTypes.shape({
-      value: PropTypes.string.isRequired,
-    })),
-    color: PropTypes.string,
-  })),
-  handleAdd: PropTypes.func.isRequired,
-  handleRemove: PropTypes.func.isRequired,
-  handleUpdate: PropTypes.func.isRequired,
-};
-
-export default props;
