43592daedc808b8b8f5834da516d956d44053975
Daniel Kemter <danne2170@gmail.com>
2017-10-06 09:55:49 +0200

    15_whiteboard-wrapper (#13)
    * 15_notes-wrapper-props
    
    * 15_whiteboard-wrapper
    
    *changes in wrapper.props needs to be done

---
+++ src/common/WhiteboardContainer/WhiteboardWrapper.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import NoteList from '../components/NoteList';
+import WhiteboardWrapperProps from './WhiteboardWrapper.props';
+import InfoComponent from '../components/Info';
+import AddComponent from '../components/Add';
+
+const WhiteboardWrapper = props => (
+  <div className="NoteContainer-wrapper">
+    <InfoComponent logo="../img/logo.svg" />
+    <NoteList
+      notes={props.notes}
+      handleRemove={props.handleRemove}
+    />
+    <AddComponent />
+  </div>
+);
+
+WhiteboardWrapper.propTypes = WhiteboardWrapperProps;
+
+export default WhiteboardWrapper;
