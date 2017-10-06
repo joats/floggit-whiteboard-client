faa9e64305b3ccb29a4c0c59ca46d3bfcd874cc2
Robert Savela <strobbann@gmail.com>
2017-10-06 15:08:15 +0200

    another merge fix

--- src/common/components/Add/AddComponent.js
+++ src/common/components/Add/AddComponent.js
@@ -4,11 +4,7 @@ import addComponentProps from './AddComponent.props';
 
 const Add = (props) => {
   const handleClick = () => {
-<<<<<<< HEAD
     props.onAdd({ title: 'hdhdh', infoList: ['mdmd'] });
-=======
-    props.onAdd({ title: 'hdhdh', infoList:['mdmd'] });
->>>>>>> 2569e04b198798ff71994a3c7230ca3d7ef57108
   };
 
   return (
